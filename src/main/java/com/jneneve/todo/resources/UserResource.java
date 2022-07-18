33d1f8a23ff3c7468a26af5723ded304992fd470
jneneve <josueneneve@hotmail.com>
2022-07-18 07:41:19 -0300

    refactor: methods and path variable from UserResource

--- src/main/java/com/jneneve/todo/resources/UserResource.java
+++ src/main/java/com/jneneve/todo/resources/UserResource.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.jneneve.todo.entities.Task;
 import com.jneneve.todo.entities.User;
 import com.jneneve.todo.services.UserService;
 
@@ -27,45 +26,45 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		List<User> list = service.findAll();
+	public ResponseEntity<List<User>> getAllUsers() {
+		List<User> list = service.getAllUsers();
 		return ResponseEntity.ok().body(list);
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User obj = service.findById(id);
+	public ResponseEntity<User> getUser(@PathVariable Long id) {
+		User obj = service.getUser(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
 	@PostMapping
-	public ResponseEntity<User> insert(@RequestBody User obj) {
-		obj = service.insert(obj);
+	public ResponseEntity<User> addUser(@RequestBody User obj) {
+		obj = service.addUser(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
 
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id) {
-		service.delete(id);
+	public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+		service.deleteUser(id);
 		return ResponseEntity.noContent().build();
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
-		obj = service.update(id, obj);
+	public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.updateUser(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 
-	@PutMapping(value = "/{id}/addtask")
-	public ResponseEntity<User> addTask(@PathVariable Long id, @RequestBody Task obj) {
-		User user = service.addTask(id, obj);
+	@PutMapping(value = "/{idUser}/addtask/{idTask}")
+	public ResponseEntity<User> addTaskToUser(@PathVariable Long idUser, @PathVariable Long idTask) {
+		User user = service.addTaskToUser(idUser, idTask);
 		return ResponseEntity.ok().body(user);
 	}
 
-	@DeleteMapping(value = "/{user_id}/deletetask/{task_id}")
-	public ResponseEntity<Void> deleteTask(@PathVariable Long user_id, @PathVariable Long task_id) {
-		service.deleteTask(user_id, task_id);
+	@DeleteMapping(value = "/{idUser}/deletetask/{idTask}")
+	public ResponseEntity<Void> deleteTaskFromUser(@PathVariable Long idUser, @PathVariable Long idTask) {
+		service.deleteTaskFromUser(idUser, idTask);
 		return ResponseEntity.noContent().build();
 	}
 }
